13359cfbf2e4a9b1262f1f1a2fa37af2c08ecd7f
Gabriel Diegel <gabriel.diegel@gmail.com>
2015-02-21 14:27:30 +0100

    Initial commit

---
+++ src/main/java/io/fs39/App.java
@@ -0,0 +1,13 @@
+package io.fs39;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
